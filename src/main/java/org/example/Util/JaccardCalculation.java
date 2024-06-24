13d060335f0fa019f305b796e604a17694f7516f
Andrew Zaika <lytiy7404@gmail.com>
2024-06-24 17:56:52 +0300

    Fixed JaccardCalculation now its correctly calculates

--- src/main/java/org/example/Util/JaccardCalculation.java
+++ src/main/java/org/example/Util/JaccardCalculation.java
@@ -4,10 +4,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class JaccardCalculation {
-    public static double jaccardSimilarity(Set<String> set1, Set<String> set2){
-        Set<String> intersection = new HashSet<>(set1);
+    public static double jaccardSimilarity(Set<Character> set1, Set<Character> set2){
+        Set<Character> intersection = new HashSet<>(set1);
         intersection.retainAll(set2);
-        Set<String> union = new HashSet<>();
+        Set<Character> union = new HashSet<>(set1);
         union.addAll(set2);
         return (double) intersection.size() / union.size();
 
