5c96525f2c9d84667a6b1577f32aecc361ce4994
Andrew Zaika <lytiy7404@gmail.com>
2024-06-26 15:42:58 +0300

    *refactoring removed all try-catch in JsonFileReader.java add to signature of method

--- src/main/java/org/example/Util/IO/JsonFileReader.java
+++ src/main/java/org/example/Util/IO/JsonFileReader.java
@@ -2,7 +2,6 @@ package org.example.Util.IO;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.example.Util.Reader;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,37 +12,25 @@ import java.util.List;
 
 public class JsonFileReader implements Reader {
     @Override
-    public String read(String filePath) {
-        try {
+    public String read(String filePath) throws IOException {
             return new String(Files.readAllBytes(Paths.get(filePath)));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
-    public HashMap<String, String> readJsonToHashMap(String filePath) {
+    public HashMap<String, String> readJsonToHashMap(String filePath) throws IOException {
         HashMap<String, String> map;
-        try {
             String jsonContent = read(filePath);
             ObjectMapper objectMapper = new ObjectMapper();
             map = objectMapper.readValue(jsonContent, new TypeReference<>() {});
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
         return map;
     }
 
 
     // Метод для читання JSON-файлу і перетворення його на мапу
-    public HashMap<String, List<String>> readJsonObjectArrayToMap(String filePath) {
+    public HashMap<String, List<String>> readJsonObjectArrayToMap(String filePath) throws IOException {
         LinkedHashMap<String, List<String>> map;
-        try {
-            String jsonContent = new String(Files.readAllBytes(Paths.get(filePath)));
-            ObjectMapper objectMapper = new ObjectMapper();
-            map = objectMapper.readValue(jsonContent, new TypeReference<LinkedHashMap<String, List<String>>>() {});
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        String jsonContent = new String(Files.readAllBytes(Paths.get(filePath)));
+        ObjectMapper objectMapper = new ObjectMapper();
+        map = objectMapper.readValue(jsonContent, new TypeReference<LinkedHashMap<String, List<String>>>() {});
         return map;
     }
 }
