a05a26116252e5df507a104298200cb372d8ec3b
Andrew Zaika <lytiy7404@gmail.com>
2024-06-26 15:53:29 +0300

    *refactoring PREVIOUS COMMIT WAS A MISTAKE added comments to ProductPosition.java

--- src/main/java/org/example/Product/ProductPosition.java
+++ src/main/java/org/example/Product/ProductPosition.java
@@ -4,7 +4,7 @@ package org.example.Product;
  * This class represents a product position in an inventory or order.
  * It contains various attributes such as article, product name, sizes, trademark, country of origin,
  * quantity, composition, gender, HS code, brutto weight, and price.
- *
+ * <p>
  * The class also provides a builder pattern for creating instances of ProductPosition.
  */
 public class ProductPosition {
@@ -24,23 +24,26 @@ public class ProductPosition {
      * Default constructor for ProductPosition.
      * Initializes all attributes to default values.
      */
-    public ProductPosition(){};
+    public ProductPosition() {
+    }
+
+    ;
 
     /**
      * Constructor for ProductPosition.
      * Initializes all attributes with the provided values.
      *
-     * @param article the article of the product
-     * @param productName the name of the product
-     * @param sizes the sizes of the product
-     * @param tradeMark the trademark of the product
+     * @param article       the article of the product
+     * @param productName   the name of the product
+     * @param sizes         the sizes of the product
+     * @param tradeMark     the trademark of the product
      * @param countryOrigin the country of origin of the product
-     * @param quantity the quantity of the product
-     * @param composition the composition of the product
-     * @param gender the gender of the product
-     * @param hsCode the HS code of the product
-     * @param bruttoWeight the brutto weight of the product
-     * @param price the price of the product
+     * @param quantity      the quantity of the product
+     * @param composition   the composition of the product
+     * @param gender        the gender of the product
+     * @param hsCode        the HS code of the product
+     * @param bruttoWeight  the brutto weight of the product
+     * @param price         the price of the product
      */
     public ProductPosition(String article, String productName,
                            String sizes, String tradeMark,
@@ -61,36 +64,165 @@ public class ProductPosition {
     }
 
     // getters and setters for all attributes
+    public String getArticle() {
+        return article;
+    }
 
-    // builder pattern for creating instances of ProductPosition
-    public static Builder newBuilder(){
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getSizes() {
+        return sizes;
+    }
+
+    public String getTradeMark() {
+        return tradeMark;
+    }
+
+    public String getCountryOrigin() {
+        return countryOrigin;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getComposition() {
+        return composition;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public String getHsCode() {
+        return hsCode;
+    }
+
+    public double getBruttoWeight() {
+        return bruttoWeight;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setArticle(String article) {
+        this.article = article;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setSizes(String sizes) {
+        this.sizes = sizes;
+    }
+
+    public void setTradeMark(String tradeMark) {
+        this.tradeMark = tradeMark;
+    }
+
+    public void setCountryOrigin(String countryOrigin) {
+        this.countryOrigin = countryOrigin;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setComposition(String composition) {
+        this.composition = composition;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public void setHsCode(String hsCode) {
+        this.hsCode = hsCode;
+    }
+
+    public void setBruttoWeight(double bruttoWeight) {
+        this.bruttoWeight = bruttoWeight;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public static Builder newBuilder() {
         return new ProductPosition().new Builder();
     }
 
+    // builder pattern for creating instances of ProductPosition
     public class Builder {
         private Builder() {
 
         }
 
         // builder methods for setting attributes
-
         public Builder setArticle(String article) {
             ProductPosition.this.article = article;
             return this;
         }
 
-        // other builder methods for setting attributes
+        public Builder setProductName(String productName) {
+            ProductPosition.this.productName = productName;
+            return this;
+        }
+
+        public Builder setSizes(String sizes) {
+            ProductPosition.this.sizes = sizes;
+            return this;
+        }
+
+        public Builder setTradeMark(String tradeMark) {
+            ProductPosition.this.tradeMark = tradeMark;
+            return this;
+        }
+
+        public Builder setCountryOrigin(String countryOrigin) {
+            ProductPosition.this.countryOrigin = countryOrigin;
+            return this;
+        }
+
+        public Builder setQuantity(int quantity) {
+            ProductPosition.this.quantity = quantity;
+            return this;
+        }
+
+        public Builder setComposition(String composition) {
+            ProductPosition.this.composition = composition;
+            return this;
+        }
+
+        public Builder setGender(Gender gender) {
+            ProductPosition.this.gender = gender;
+            return this;
+        }
+
+        public Builder setHsCode(String hsCode) {
+            ProductPosition.this.hsCode = hsCode;
+            return this;
+        }
+
+        public Builder setBruttoWeight(double bruttoWeight) {
+            ProductPosition.this.bruttoWeight = bruttoWeight;
+            return this;
+        }
+
+        public Builder setPrice(double price) {
+            ProductPosition.this.price = price;
+            return this;
+        }
 
         public ProductPosition build() {
-            return  ProductPosition.this;
+            return ProductPosition.this;
         }
     }
 
-    /**
-     * Returns a string representation of the ProductPosition object.
-     *
-     * @return a string representation of the ProductPosition object
-     */
     @Override
     public String toString() {
         return "ProductPosition{" +
