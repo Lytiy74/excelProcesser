1655afc4f8d8d62cf0b34ece036fe695aee262f0
Andrew Zaika <lytiy7404@gmail.com>
2024-06-26 15:39:20 +0300

    *refactoring added comments to MapConverter.java

--- src/main/java/org/example/Util/MapConverter.java
+++ src/main/java/org/example/Util/MapConverter.java
@@ -4,13 +4,21 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MapConverter {
-    public static HashMap<String, String> invertColumnMap(HashMap<String, List<String>> targetColumns) {
-        HashMap<String, String> columnKeyMap = new HashMap<>();
-        for (String key : targetColumns.keySet()) {
-            for (String value : targetColumns.get(key)) {
-                columnKeyMap.put(value, key);
-            }
+    /**
+ * This method inverts a given HashMap where the key is a String and the value is a List of Strings.
+ * The inverted map will have the values from the original map as keys and the corresponding keys from the original map as values.
+ * If there are duplicate values in the original map, only the last occurrence of each value will be present in the inverted map.
+ *
+ * @param inputMap The original HashMap to invert.
+ * @return A new HashMap where the values from the original map are keys and the corresponding keys from the original map are values.
+ */
+public static HashMap<String, String> invertColumnMap(HashMap<String, List<String>> inputMap) {
+    HashMap<String, String> columnKeyMap = new HashMap<>();
+    for (String key : inputMap.keySet()) {
+        for (String value : inputMap.get(key)) {
+            columnKeyMap.put(value, key);
         }
-        return columnKeyMap;
     }
+    return columnKeyMap;
+}
 }
