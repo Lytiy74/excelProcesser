5f64d89796f5313005c143fff8a60867af9a6d45
Andrew Zaika <lytiy7404@gmail.com>
2024-06-26 16:05:39 +0300

    *refactoring  added comments to MaterialProcess.java

--- src/main/java/org/example/ProductProcess/Composition/MaterialProcess.java
+++ src/main/java/org/example/ProductProcess/Composition/MaterialProcess.java
@@ -3,23 +3,55 @@ package org.example.ProductProcess.Composition;
 import org.example.Util.IO.JsonFileReader;
 import org.example.Util.MapConverter;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 
+/**
+ * This class is responsible for processing material compositions.
+ * It reads a JSON file containing material compositions, parses the composition strings,
+ * translates them into a standard format, and builds a formatted composition string.
+ */
 public class MaterialProcess {
+    /**
+     * The path to the JSON file containing material compositions.
+     */
     private static final String COMPOSITION_JSON_FILE_PATH = "src/main/resources/compositionNew.json";
+
+    /**
+     * The parser for parsing the composition strings.
+     */
     private final MaterialParser parser;
+
+    /**
+     * The translator for translating material names.
+     */
     private final MaterialTranslator translator;
+
+    /**
+     * The builder for building the formatted composition string.
+     */
     private final MaterialStringBuilder materialStringBuilder;
 
-    public MaterialProcess() {
+    /**
+     * Constructs a new MaterialProcess object.
+     *
+     * @throws IOException if an error occurs while reading the JSON file.
+     */
+    public MaterialProcess() throws IOException {
         HashMap<String, List<String>> stringStringHashMap = new JsonFileReader().readJsonObjectArrayToMap(COMPOSITION_JSON_FILE_PATH);
         parser = new MaterialParser();
         translator = new MaterialTranslator(MapConverter.invertColumnMap(stringStringHashMap));
         materialStringBuilder = new MaterialStringBuilder();
     }
 
+    /**
+     * Generates a formatted composition string based on the given composition string.
+     *
+     * @param composition the composition string to be processed.
+     * @return the formatted composition string.
+     */
     public String generateCompositionString(String composition) {
         LinkedHashMap<String, Integer> compositionMap = parser.parseStringCompositionToMap(composition);
         LinkedHashMap<String, Integer> translatedMap = new LinkedHashMap<>();
