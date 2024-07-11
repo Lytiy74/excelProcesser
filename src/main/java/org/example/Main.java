bf8887c8d3bff4a100ffc403ac853f4de1d1858a
Andrew Zaika <lytiy7404@gmail.com>
2024-07-11 13:47:56 +0300

    Added interface MaterialProcess.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,18 @@
 package org.example;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.example.ExcelProcesser.ExcelProcess;
 import org.example.Product.ProductPosition;
-import org.example.ProductProcess.Composition.MaterialProcess;
+import org.example.Product.ProductProcess.Composition.MaterialProcess;
+import org.example.Product.ProductProcess.Composition.MaterialProcessImpl;
 import org.example.Util.IO.ExcelFileWriter;
 import org.example.Util.IO.JsonFileReader;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -18,17 +20,19 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         JsonFileReader reader = new JsonFileReader();
-        MaterialProcess materialProcess = new MaterialProcess();
+        MaterialProcess materialProcess = new MaterialProcessImpl();
         HashMap<String, List<String>> stringStringHashMap = reader.readJsonObjectArrayToMap("src/main/resources/columnNames.json");
-        Workbook workbook = new XSSFWorkbook("src/main/resources/test2.xlsx");
-        ExcelProcess excelProcess = new ExcelProcess(workbook,0,stringStringHashMap);
+        Workbook workbook = new XSSFWorkbook("src/main/resources/опис.xlsx");
+        ExcelProcess excelProcess = new ExcelProcess(workbook, 0, stringStringHashMap);
         HashMap<String, ProductPosition> productPositionHashMap = excelProcess.collectProducts();
-        for (String article: productPositionHashMap.keySet()){
-           ProductPosition productPosition =  productPositionHashMap.get(article);
-           productPosition.setComposition(materialProcess.generateCompositionString(productPosition.getComposition()));
+        for (String article : productPositionHashMap.keySet()) {
+            ProductPosition productPosition = productPositionHashMap.get(article);
+            String comp = materialProcess.generateCompositionString(productPosition.getComposition());
+            productPosition.setComposition(comp);
         }
         excelProcess.addProductsToSheet(productPositionHashMap, stringStringHashMap.keySet().stream().toList());
         ExcelFileWriter writer = new ExcelFileWriter();
-        writer.write(workbook,"src/main/resources/result.xlsx");
+        writer.write(workbook, "src/main/resources/result.xlsx");
+
     }
 }
