b1e495f99d9e01467da35dbe273fe80cf23a43e2
Andrew Zaika <lytiy7404@gmail.com>
2024-07-11 13:09:31 +0300

    Added junitTest

---
+++ src/test/java/MaterialProcessTest.java
@@ -0,0 +1,44 @@
+import org.example.Product.ProductProcess.Composition.MaterialProcess;
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+
+public class MaterialProcessTest {
+    static MaterialProcess materialProcess;
+    @BeforeAll
+    static void setup() throws IOException {
+        materialProcess = new MaterialProcess();
+    }
+    @DisplayName("Test with normal string")
+    @Test
+    void normalStringOfComposition(){
+        String translated = materialProcess.generateCompositionString("50% CO 40% WO 10% PA");
+        Assertions.assertEquals("50% бавовна 40% вовна 10% поліамід", translated);
+    }
+    @DisplayName("Test summing with same fibres")
+    @Test
+    void sumSameFibres(){
+        String translated = materialProcess.generateCompositionString("50% CO 20% WO 20% WV 10% PA");
+        Assertions.assertEquals("50% бавовна 40% вовна 10% поліамід", translated);
+    }
+
+    @DisplayName("Test with unsorted percentage")
+    @Test
+    void unsortedPercentage(){
+        String translated = materialProcess.generateCompositionString("10% WO 50% CO 40% PA");
+        Assertions.assertEquals("50% бавовна 40% поліамід 10% вовна", translated);
+    }
+
+    @Nested
+    class TranslateMaterial {
+        @DisplayName("Test with translation")
+        @Test
+        void translateMaterialTest(){
+            String translated = materialProcess.translateComposition("CO");
+            Assertions.assertEquals("бавовна", translated);
+            translated = materialProcess.translateComposition("WO");
+            Assertions.assertEquals("вовна", translated);
+        }
+
+    }
+}
