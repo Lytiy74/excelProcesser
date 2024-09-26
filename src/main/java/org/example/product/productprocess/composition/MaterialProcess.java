1d41cc9dbdab687150e5544a9986e542d08f9ae2
Andrew Zaika <lytiy7404@gmail.com>
2024-09-26 14:24:38 +0300

    Fix typo in method parameter name
    Corrected the parameter name from 'compositon' to 'composition' in the translateComposition method of the MaterialProcess interface to ensure consistency and avoid potential errors. This change enhances code readability and reduces the likelihood of bugs.

--- src/main/java/org/example/product/productprocess/composition/MaterialProcess.java
+++ src/main/java/org/example/product/productprocess/composition/MaterialProcess.java
@@ -2,5 +2,5 @@ package org.example.product.productprocess.composition;
 
 public interface MaterialProcess {
     String generateCompositionString(String composition);
-    String translateComposition(String compositon);
+    String translateComposition(String composition);
 }
