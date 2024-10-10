f8f5bf5a464920ab767436975f076c0e1ae305d7
Andrew Zaika <lytiy7404@gmail.com>
2024-10-10 17:39:06 +0300

    reduce quantity of parameters in ExcelProductReader.java which extends AbstractExcelProductReader.java

--- src/main/java/org/example/excelprocessor/ExcelProductReader.java
+++ src/main/java/org/example/excelprocessor/ExcelProductReader.java
@@ -1,12 +1,7 @@
 package org.example.excelprocessor;
 
-import org.example.product.productprocess.ProductProcess;
-
-import java.util.HashMap;
-import java.util.List;
-
 public class ExcelProductReader extends AbstractExcelProductReader implements IExcelProductReader{
-    public ExcelProductReader(IExcelProductBuilder productBuilder, ProductProcess productProcess, HashMap<String, List<String>> targetColumns) {
-        super(productBuilder, productProcess, targetColumns);
+    public ExcelProductReader(IExcelProductBuilder productBuilder, int headerRowIndex) {
+        super(productBuilder, headerRowIndex);
     }
 }
