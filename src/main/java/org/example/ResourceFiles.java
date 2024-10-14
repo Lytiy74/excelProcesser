f6b9f0bbaff24f922da3d05815724a1fd1bf1576
Andrew Zaika <lytiy7404@gmail.com>
2024-10-14 14:44:19 +0300

    renamed to productMetas

--- src/main/java/org/example/ResourceFiles.java
+++ src/main/java/org/example/ResourceFiles.java
@@ -3,7 +3,7 @@ package org.example;
 public enum ResourceFiles {
     COLUMN_NAME_JSON_FILE("columnNames.json"),
     COMPOSITION_JSON_FILE("compositionNew.json"),
-    CLOTHES_NAMES_JSON_FILE("clothesNames.json"),
+    CLOTHES_NAMES_JSON_FILE("productMetas.json"),
     CLOTHES_SIZE_JSON_FILE("clothesSize.json");
     private String fileName;
 
