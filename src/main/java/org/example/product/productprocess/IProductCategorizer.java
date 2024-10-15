484205070bd6cc5f6e412073afd7e66bf8743a4c
Andrew Zaika <lytiy7404@gmail.com>
2024-10-15 17:25:11 +0300

    Added interface for ProductCategorizer

---
+++ src/main/java/org/example/product/productprocess/IProductCategorizer.java
@@ -0,0 +1,7 @@
+package org.example.product.productprocess;
+
+import org.example.product.ProductType;
+
+public interface IProductCategorizer {
+    ProductType categorizeProduct(String productName);
+}
--- src/main/java/org/example/product/productprocess/ProductCategorizer.java
+++ src/main/java/org/example/product/productprocess/ProductCategorizer.java
@@ -5,13 +5,14 @@ import org.example.product.ProductType;
 
 import java.util.HashMap;
 
-public class ProductCategorizer {
-    private HashMap<String, ProductMeta> productMetaHashMap;
+public class ProductCategorizer implements IProductCategorizer{
+    private final HashMap<String, ProductMeta> productMetaHashMap;
 
     public ProductCategorizer(HashMap<String, ProductMeta> productMetaHashMap) {
         this.productMetaHashMap = productMetaHashMap;
     }
 
+    @Override
     public ProductType categorizeProduct(String productName){
         ProductMeta productMeta = productMetaHashMap.get(productName);
         return productMeta != null ? productMeta.getProductType() : ProductType.OTHERS;
