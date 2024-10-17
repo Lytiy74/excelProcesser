5bc900b9d0b71923de66ca5cbdfa40d795957560
Andrew Zaika <lytiy7404@gmail.com>
2024-10-17 15:41:12 +0300

    changed package

--- src/main/java/org/lytiy/util/io/JsonFileReader.java
+++ src/main/java/org/lytiy/util/io/JsonFileReader.java
@@ -3,8 +3,8 @@ package org.lytiy.util.io;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.lytiy.product.productprocess.ProductMeta;
-import org.lytiy.product.productprocess.commodity.CommodityItem;
+import org.lytiy.cargo.product.productprocess.ProductMeta;
+import org.lytiy.cargo.product.productprocess.commodity.CommodityItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
